c25df78374b9173e2595d90b635ad116b72a2281
Hemanth <hemanthaim543210@gmail.com>
2023-08-12 17:34:45 +0530

    home screen big change

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,29 +1,65 @@
 import React from "react";
-import {View,Text,StyleSheet,ScrollView,SafeAreaView, FlatList} from "react-native";
-import {notesTemp} from "../other/notesTestData";
-import {hashColor} from "../other/notesTestData"
-const Home=()=>{
-    return(
-        <View style={styles.container}>
-           <SafeAreaView>
-            <ScrollView>
-                <FlatList
-                data={notesTemp}
-                keyExtractor={(item)=>item.id}
-                />
-            </ScrollView>
-           </SafeAreaView>
-        </View>
-    )
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  SafeAreaView,
+  FlatList,
+} from "react-native";
+import { notesTemp } from "../other/notesTestData";
+import { hashColor } from "../other/notesTestData";
+
+const renderNotesMini = ({ item }) => {
+  return (
+    <View style={[styles.miniNotesContainer, {}]}>
+      <Text>{item.title}</Text>
+      <Text>{item.notes.slice(0,80)}</Text>
+    </View>
+  );
 };
 
-const styles=StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:"center",
-        alignItems:"center",
-        backgroundColor:"#FCFCFC"
-    }
-})
+const Home = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.scrollContainer}>
+          <FlatList
+            data={notesTemp}
+            keyExtractor={(item) => item.id}
+            renderItem={renderNotesMini}
+            numColumns={2}
+            columnWrapperStyle={styles.columnWrapper}
+
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FCFCFC",
+  },
+  miniNotesContainer: {
+    backgroundColor:"#FFAB91",
+    width:170,
+    height:170,
+    margin:10,
+    borderRadius:20,
+    padding:10,
+  },
+  scrollContainer:{
+
+  }, columnWrapper: {
+    justifyContent: "space-between", // This will create spacing between columns
+    paddingHorizontal: 10
+     // This will create spacing between columns
+  },
+});
 
 export default Home;
